23da76b5c8a441f25c6eb21d33b3ec381cf55f77
giuy23 <sabesqcolgate@gmail.com>
2026-01-16 23:11:50 -0500

    [Added] Experience tag

---
+++ src/utils/constans.ts
@@ -0,0 +1,78 @@
+import Tailwind from "../icons/Tailwind.astro";
+import Vue from "../icons/Vue.astro";
+import React from "../icons/React.astro";
+import Laravel from "../icons/Laravel.astro";
+import Supabase from "../icons/Supabase.astro";
+import MySQL from "../icons/MySQL.astro";
+import Python from "../icons/Python.astro";
+
+export const TAGS = {
+  TAILWIND: {
+    name: "Tailwind CSS",
+    class: "bg-black text-white",
+    icon: Tailwind,
+  },
+  VUE: {
+    name: "Vue JS",
+    class: "bg-black text-white",
+    icon: Vue,
+  },
+  REACT: {
+    name: "React",
+    class: "bg-black text-white",
+    icon: React,
+  },
+  LARAVEL: {
+    name: "Laravel",
+    class: "bg-black text-white",
+    icon: Laravel,
+  },
+  SUPABASE: {
+    name: "Supabase",
+    class: "bg-black text-white",
+    icon: Supabase,
+  },
+  MySQL: {
+    name: "MySQL",
+    class: "bg-black text-white",
+    icon: MySQL,
+  },
+};
+
+export const SKILLS = [
+  {
+    name: "Tailwind CSS",
+    class: "bg-black text-white",
+    icon: Tailwind,
+  },
+  {
+    name: "Vue JS",
+    class: "bg-black text-white",
+    icon: Vue,
+  },
+  {
+    name: "React",
+    class: "bg-black text-white",
+    icon: React,
+  },
+  {
+    name: "Laravel",
+    class: "bg-black text-white",
+    icon: Laravel,
+  },
+  {
+    name: "Supabase",
+    class: "bg-black text-white",
+    icon: Supabase,
+  },
+  {
+    name: "MySQL",
+    class: "bg-black text-white",
+    icon: MySQL,
+  },
+  {
+    name: "Python",
+    class: "bg-black text-white",
+    icon: Python,
+  }
+];
